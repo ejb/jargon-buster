ff37dd09ff202843fab8f809e3cb2248f09d2ae8
ejb <mail@elliotbentley.com>
2015-03-28 17:09:44 +0000

    Very dumb term identification

---
+++ app/scripts/jargon-buster-extension.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+  var buster = new JargonBuster();
+}
+
--- app/scripts/jargon-buster.js
+++ app/scripts/jargon-buster.js
@@ -1,3 +1,37 @@
-window.onload = function(){
-  alert('Jargon begone!');
+'use strict';
+
+function JargonBuster(){
+  var that = this;
+  this.getDictionary(function(dict){
+    that.dictionary = dict;
+    var tags = that.findTextBlocks();
+    that.findJargon( tags, dict );
+  });
 }
+
+JargonBuster.prototype.getDictionary = function(callback){
+  var url = chrome.extension.getURL('/dictionary.json');
+  $.getJSON( url, callback );
+}
+
+JargonBuster.prototype.findTextBlocks = function(){
+  return $('p');
+}
+
+JargonBuster.prototype.findJargon = function($tags, dict){
+  // console.log(tags);
+    for (var j = 0; j < dict.length; j++) {
+      var term = dict[j].term;
+      // console.log( tags[i], tags[i].innerText);
+      var withTag = '<span class="jargon-buster-term" style="color: red;">'+term+'</span>';
+      
+      $tags.html(function () {
+          return $(this).html().replace(term, withTag); 
+      });
+            
+      // TODO:
+      // - If link, ignore
+      // - Retain case
+    }
+}
+
